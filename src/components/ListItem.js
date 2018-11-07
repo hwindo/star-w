79769b5e2e9857bead1ae002b5b731a178cd4474
Herwindo Artono <herwindo.artono@gmail.com>
2018-11-07 13:18:59 +0700

    quick fix on regex link styles

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,24 +1,38 @@
 import React, {Component} from 'react';
 import BookmarkBtn from './BookmarkBtn';
+import {NavLink} from "react-router-dom";
+import {extractResource} from "../helper";
 
 class ListItem extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            resource: this.props.resource,
+            id: ''
+        }
     }
 
     get title() {
         return this.props.data.name ? this.props.data.name : this.props.data.title;
     }
 
+
+
     render() {
         return (
             <li className='list-item'>
+
                 <div className="resource-tag">
                     <div className="title">{this.props.resource}</div>
                     <div className='block'> </div>
                 </div>
-                <div className="title">{this.title}</div>
+                <div className="title">
+                    <NavLink to={'/' + extractResource(this.props.data.url).resource + '/' + extractResource(this.props.data.url).id}>
+                        {this.title}
+                    </NavLink>
+                </div>
                 <BookmarkBtn resource={this.props.resource} />
+
             </li>
         )
     }
